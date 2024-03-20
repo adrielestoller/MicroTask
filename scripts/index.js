5a5b1838a8f4c0004b9887026ec7bb68a3b5d1b3
Adriel <adrielestoller@gmail.com>
2024-03-20 15:26:24 -0300

    :tada: build: migration to nextjs 14

--- scripts/index.js
+++
@@ -1,7 +0,0 @@
-  function changeTaskStatus(clickedIcon) {
-    if(clickedIcon.classList.contains("fa-regular")) {
-      clickedIcon.classList.replace("fa-regular", "fa-solid");
-    } else {
-      clickedIcon.classList.replace("fa-solid", "fa-regular");
-    }
-  }
